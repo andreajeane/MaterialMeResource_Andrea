b1773cf4946cd53537dcc72866c0425a59a13f8d
Andrea Jeane Elquita <andreajeane28.aje@gmail.com>
2022-12-31 00:30:44 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/andrea/materialmeresource_andrea/DetailActivity.java
@@ -0,0 +1,32 @@
+package id.ac.poliban.andrea.materialmeresource_andrea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Initialize the views.
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        // Set the text from the Intent extra.
+        sportsTitle.setText(getIntent().getStringExtra("title"));
+
+        // Load the image using the Glide library and the Intent extra.
+        Glide.with(this)
+                .load(getIntent()
+                        .getIntExtra("image_resource",0))
+                .into(sportsImage);
+    }
+}
